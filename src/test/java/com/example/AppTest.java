902a3bc2e7e420bcdea551c144edce52346cba5f
Valentin_cabrel22 <cabreltaya@gmail.com>
2024-09-12 14:18:41 +0200

    add class appTest

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,17 +4,25 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void test_add_to_chart()
     {
         assertTrue( true );
     }
+    @Test
+    public void test_login_feature()
+    {
+        assertTrue( false );
+    }
+    @Test
+    public void test_sign_feature()
+    {
+        assertTrue( true );
+    }
+
 }
